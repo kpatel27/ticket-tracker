22e1972da1a00a668c99b8625cc23041a48c7f04
Krunal Patel <kppatel727@gmail.com>
2020-04-28 20:44:25 -0700

    show a projects tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,6 @@ class Ticket < ApplicationRecord
   validates :name, presence: true
   validates :status, inclusion: { in: ['new', 'blocked', 'in_progress', 'fixed']}
   validates :project_id, presence: true
-  
+
   belongs_to :project
 end
